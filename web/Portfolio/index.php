62ca648b0b5fdb5ff8bcc7bd5b3258c2983e1e3f
Brittany Hill <brinbh@ldschurch.org>
2018-02-28 21:16:10 -0700

    testing

--- web/Portfolio/index.php
+++ web/Portfolio/index.php
@@ -68,10 +68,7 @@
                 echo "<p>TEST</p>";
                 for ($i = 0; $i < 5; $i++) {
                     echo "testing";
-                    echo $projects;
-                    echo $projects['project_title'];
-                    echo $projects[$i][project_title];
-                    echo $projects[$i]['project_title'];
+                    echo $projects[$i];
                 }
                 foreach ($projects as $project) {
                 echo "index.php $project";
